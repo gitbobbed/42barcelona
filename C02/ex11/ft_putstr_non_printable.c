2a8538e2ba887a2965d6da8ae6a95d552112988d
Daniel Pereira Maciel <danielpereiramaciel@Daniels-MacBook-Pro.local>
2024-11-02 23:06:26 +0100

    Syncing Gits

--- C02/ex11/ft_putstr_non_printable.c
+++ C02/ex11/ft_putstr_non_printable.c
@@ -6,7 +6,7 @@
 /*   By: dpereir2 <dpereir2@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/31 23:14:16 by dpereir2          #+#    #+#             */
-/*   Updated: 2024/10/31 23:14:19 by dpereir2         ###   ########.fr       */
+/*   Updated: 2024/11/02 22:56:07 by dpereir2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ void	ft_putchar(char c)
 	write(1, &c, 1);
 }
 
-void	ft_putstr_non_printable (char *str)
+void	ft_putstr_non_printable(char *str)
 {
 	int				i;
 	char			*hex;
-	
+
 	i = 0;
 	hex = "0123456789abcdef";
 	while (str[i] != '\0')
 	{
-		if (str[i] < 32 || str[i] > 126) 
+		if (str[i] < 32 || str[i] > 126)
 		{
 			ft_putchar('\\');
 			ft_putchar(hex[str[i] / 16]);
@@ -34,6 +34,21 @@ void	ft_putstr_non_printable (char *str)
 		}
 		else
 			ft_putchar(str[i]);
-		str++;
+		i++;
 	}
 }
+/*
+int	main(void)
+{
+	char	*str0;
+	char	*str1;
+	char	*str2;
+
+	str0 = "Hello\nWorld";
+	str1 = "T\tabc";
+	str2 = "\\";
+	ft_putstr_non_printable(str0);
+	ft_putstr_non_printable(str1);
+	ft_putstr_non_printable(str2);
+}
+*/
